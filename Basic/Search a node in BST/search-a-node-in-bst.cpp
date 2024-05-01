f3938fdfc03ada953bc7a05ba193bf7df4c748ea
Aman Verma <aka.amanverma@gmail.com>
2024-05-02 00:27:28 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Search a node in BST/search-a-node-in-bst.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+bool search(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << search(root, s);
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+bool search(Node* root, int x) {
+       
+    
+        while(root != NULL ){
+            // Check if the target value is
+            // less than the current node's value.
+            // If so, move to the left subtree
+            // (values smaller than the current node).
+            // Otherwise, move to the right subtree
+            // (values larger than the current node).
+            // Node* ans = x < root->data ? root->left : root->right;
+            if(x==root->data){
+                return true;
+            }else if (x<root->data){
+                root=root->left;
+            }else{
+                root=root->right;
+            }
+        }
+        // Return the node containing the target value,
+        // if found; otherwise, return NULL.
+        
+        return false;
+}
